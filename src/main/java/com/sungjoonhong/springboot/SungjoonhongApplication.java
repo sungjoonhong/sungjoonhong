0208585785ad2ec98202c8f6f0c5496cae35d180
Sungjoon Hong <sungjoon.hong.46@gmail.com>
2019-09-04 02:07:10 -0400

    New - Spring Starter Project maven project start

---
+++ src/main/java/com/sungjoonhong/springboot/SungjoonhongApplication.java
@@ -0,0 +1,13 @@
+package com.sungjoonhong.springboot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SungjoonhongApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SungjoonhongApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/sungjoonhong/springboot/SungjoonhongApplicationTests.java
@@ -0,0 +1,16 @@
+package com.sungjoonhong.springboot;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class SungjoonhongApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
