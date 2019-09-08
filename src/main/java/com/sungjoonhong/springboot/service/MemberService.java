70fecb7f4d73e49a1b40b7ccd57b18068fdcc2ae
Sungjoon Hong <sungjoon.hong.46@gmail.com>
2019-09-07 22:52:44 -0400

    modified service add selectMemberList2 to test

--- src/main/java/com/sungjoonhong/springboot/service/MemberService.java
+++ src/main/java/com/sungjoonhong/springboot/service/MemberService.java
@@ -5,19 +5,21 @@ import java.util.List;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
-import com.sungjoonhong.springboot.dto.Member;
-import com.sungjoonhong.springboot.mapper.MemberMapper;
+import com.sungjoonhong.springboot.dao.MemberMapper;
+import com.sungjoonhong.springboot.domain.MemberVO;
 
 @Service
 public class MemberService {
-	
+
 	@Autowired
 	MemberMapper memberMapper;
-    
-    public List<Member> selectMemberList() throws Exception{
-        return memberMapper.selectMemberList();
-    }
 
-
+	public List<MemberVO> selectMemberList() throws Exception {
+		return memberMapper.selectMemberList();
+	}
+	
+	public List<MemberVO> selectMemberList2() throws Exception {
+		return memberMapper.selectMemberList();
+	}
 
 }
