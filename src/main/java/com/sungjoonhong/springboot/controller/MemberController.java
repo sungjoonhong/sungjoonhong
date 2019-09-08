7f6551a0f8082d2722c8d9c36975b3a194ca0eb8
Sungjoon Hong <sungjoon.hong.46@gmail.com>
2019-09-07 22:50:00 -0400

    modified controller

--- src/main/java/com/sungjoonhong/springboot/controller/MemberController.java
+++ src/main/java/com/sungjoonhong/springboot/controller/MemberController.java
@@ -1,29 +1,27 @@
 package com.sungjoonhong.springboot.controller;
 
-
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.servlet.ModelAndView;
 
-import com.sungjoonhong.springboot.dto.Member;
+import com.sungjoonhong.springboot.domain.MemberVO;
 import com.sungjoonhong.springboot.service.MemberService;
 
 @Controller
 public class MemberController {
+
 	@Autowired
-    MemberService memberService;
-	
+	MemberService memberService;
+
 	@RequestMapping("/member")
-    public @ResponseBody List<Member> query() throws Exception{
-        return memberService.selectMemberList();
-    }
-
-
-
-
-	
+	public ModelAndView query() throws Exception {
+		List<MemberVO> memberList = memberService.selectMemberList();
+		ModelAndView member = new ModelAndView("member");
+		member.addObject("memberList", memberList);
+		return member;
+	}
 
 }
